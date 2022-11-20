bdcd47be51e85c31fef06b1658613ab715308746
cod3burrito <robintanner44@gmail.com>
2022-11-20 16:12:26 -0500

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # wonderPasswordGen
+A password randomizer made using JavaScript!
+
+
+## Installation
+No install necessary! Just click the [link here](url) to use this app in your favorite web browser.
+
+## Credits
+With love and gratitude to my lord and savior Stack Overflow as well as my tutor!
+
+![screenshot of deployment](Capture.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,71 @@ function getValue() {
   var characters = prompt("How many characters would you like?")
   let getValue = prompt ("How many characters would you like?");
   let text;
-  if (getValue >= 7 || getValue >= 129) {
+  if (getValue <= 7 || getValue >= 129) {
     text = "Password must be between 8 and 128 characters."
   } else {
     text = "Great! Your password will be " + getValue + "characters.";
   }
 };
-function getCharacters() {
-  let getCharacters = prompt("Would you like upper or lowercase characters?")
+
+var upperCase = confirm("Would you like to include Uppercase Characters?");
+if (upperCase === true) {
+  acceptableChars = acceptableChars.concat(uppers);
 }
-// I'm overcomplicating this again
-// going to touch grass and think about this
+
+var lowerCase = confirm("Would you like to include Lowercase Characters?");
+if (lowerCase === true) {
+  acceptableChars = acceptableChars.concat(lowers);
+}
+
+var numberChars = confirm("Would you like to include Numbers?");
+if (numberChars === true) {
+  acceptableChars = acceptableChars.concat(numbers);
+}
+
+var specialChars = confirm("Would you like to include Special Characters?");
+if (specialChars === true) {
+  acceptableChars = acceptableChars.concat(symbols);
+}
+
+if (acceptableChars.length == 0) { // //Was at least one character type selected?
+  alert("Please select at least one character type.");
+  return "Please Try Again";
+}
+// User Inputs Complete.  var length has character count.  other vars have true or false selected.
+
+// Array acceptableChars now contains all possible characters for random selection.
+
+// create loop to randomly draw from acceptableChars length number of times.
+var passwordString = "";
+
+for (i = 0; i < length; i++) {
+  var randomPosition = Math.floor(Math.random() * acceptableChars.length);
+  console.log(randomPosition);
+  
+  passwordString = passwordString + acceptableChars[randomPosition]; //add new random character to passwordString.
+  console.log(passwordString);
+}
+
+return passwordString;
 
 
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// var lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-// var upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-// var number = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-// var Special = {'!', '@', '#', '$', '%', '^', '&', '*', '~', '?'};
+var uppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var symbols = ["!","#","$",",","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^","`","{","|","}","~"];
 
 
